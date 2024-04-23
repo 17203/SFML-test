f2240518763849b8eea6777dafffef011652db67
Eric <17203@portalcedi.edu.mx>
2024-04-23 13:38:57 -0600

    coso :D

---
+++ Rrectangle.hpp
@@ -0,0 +1,21 @@
+#ifndef rectangle_h
+#define rectangle_h
+
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Rectangle{
+private:
+    Vector2f speed;
+    Vector2f acc;
+    RectangleShape shape;
+public:
+    Rectangle(Vector2f size,RenderWindow &window);
+
+    void update();
+    void drawTo(RenderWindow &window);
+
+
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,37 +1,52 @@
 #include <SFML/Graphics.hpp>
+#include "Rrectangle.hpp"
+#include <vector>
+using namespace std;
 using namespace sf;
 
-float speedX = 1.f;
-float speedY = 1.f;
+
+vector <Rectangle>rectangulos;
+float speedX=1.f;
+float speedY=1.f;
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "SFML works!");
-    RectangleShape shape({60.f, 30.f});
-    shape.setFillColor(Color::Red);
-    window.setFramerateLimit(120);
+    
+    
+    window.setFramerateLimit(50);
+
+Rectangle rect(Vector2f(60.f,30.f),window);
 
     while (window.isOpen())
     {
         Event event;
         while (window.pollEvent(event))
         {
+            // escucha una vez cuando se presiona el click izquierdo del mouse
             if (event.type == Event::Closed)
                 window.close();
+            if (event.type==Event::MouseButtonPressed){
+                if(event.mouseButton.button== Mouse::Left){
+                    float x=float(rand()%100);
+                    float y=float(rand()%100);
+                    Rectangle r= Rectangle (Vector2f(x,y),window);
+                    rectangulos.push_back(r);
+                    r.drawTo(window);
+                }
+
+            }
         }
-        if(Mouse::isButtonPressed(Mouse::Left)){
-            shape.rotate(15);
-        }
+
+//entra al if si el click iquerido esta presionado
+        if(Mouse::isButtonPressed(Mouse::Left));
+
         window.clear();
-        if(shape.getPosition().x + shape.getSize().x >=800 || shape.getPosition().x < 0){
-        speedX *= -1;
-        shape.setFillColor({rand()%255,rand()%255,rand()%255});
+        for(auto &r: rectangulos){
+            r.update();
+            r.drawTo(window);
         }
-        if(shape.getPosition().y + shape.getSize().y >=600 || shape.getPosition().y < 0){
-        speedY *= -1;
-        shape.setFillColor({rand()%255,rand()%255,rand()%255});
-        }
-        shape.move({speedX,speedY});
-        window.draw(shape);
+        // rect.update();
+        // rect.drawTo(window);
         window.display();
     }
 
