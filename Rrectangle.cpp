6164f20c0a02106212b3f794a8277a5c9fa9444a
17203 <158208472+17203@users.noreply.github.com>
2024-05-21 12:06:40 -0600

    Update Rrectangle.cpp

--- Rrectangle.cpp
+++ Rrectangle.cpp
@@ -3,7 +3,7 @@
 Rectangle::Rectangle(Vector2f size, RenderWindow &window){
     this->shape = RectangleShape(size);
     this->shape.setPosition({Mouse::getPosition(window).x, Mouse::getPosition(window).y});
-    this->shape.setFillColor(Color::Blue);
+    this->shape.setFillColor(Color::Red);
     this->speed=Vector2f(4.f,4.f);
     this->acc=Vector2f(0.f, 1.f);
     this->shape.setOutlineThickness(3);
@@ -18,13 +18,11 @@ void Rectangle::update(){
     if(shape.getPosition().x+shape.getSize().x>=800|| shape.getPosition().x<0){
             
             speed.x*= -1;
-            this->shape.setFillColor({Uint8(rand()%255),Uint8(rand()%255),Uint8(rand()%255)});
             this->shape.setOutlineColor({Uint8(rand()%255),Uint8(rand()%255),Uint8(rand()%255)});
     }
     if(shape.getPosition().y+shape.getSize().y>=600|| shape.getPosition().y<0){
             
             this->speed.y*= -1;
-            this->shape.setFillColor({Uint8(rand()%255),Uint8(rand()%255),Uint8(rand()%255)});
             this->shape.setOutlineColor({Uint8(rand()%255),Uint8(rand()%255),Uint8(rand()%255)});
             this->shape.setPosition(this->shape.getPosition().x, 600-this->shape.getSize().y);
     }
@@ -32,4 +30,16 @@ void Rectangle::update(){
 
 void Rectangle::drawTo(RenderWindow &window){
     window.draw(this->shape);
+}
+
+void Rectangle::changeColorIfClicked(Vector2i position){
+    float x =this->shape.getPosition().x;
+    float y = this->shape.getPosition().y;
+    float w = this-> shape.getSize().x;
+    float h = this->shape.getSize().y;
+    int mouseX = position.x;
+    int mouseY = position.y;
+    if(mouseX>=x && mouseX <=x+w && mouseY>=y && mouseY<=y+h){
+        this->shape.setFillColor(Color::Green);
+    }
 }
